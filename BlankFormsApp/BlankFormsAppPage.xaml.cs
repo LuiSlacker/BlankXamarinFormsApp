0322ea318d74071dc034d1795806bdaa581ecc16
Petulant Slacker <l.goohsen@gmail.com>
2017-10-02 16:53:41 -0500

    inital commit

---
+++ BlankFormsApp/BlankFormsAppPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+using System;
+using Xamarin.Forms.Xaml;
+
+namespace BlankFormsApp {
+    public partial class BlankFormsAppPage : ContentPage {
+        public BlankFormsAppPage() {
+            InitializeComponent();
+
+            StackLayout layout = new StackLayout {
+                Margin = 20,
+            };
+
+            Button btn = new Button {
+                Text = "click"
+            };
+
+			btn.Clicked += (object sender, System.EventArgs e) => {
+                Button button = (Button)sender;
+				DisplayAlert("Clicked!", "The button labeled '" + button.Text + "' has been clicked", "OK");
+			};
+
+            Label label = new Label {
+                Text = "Hola"
+            };
+
+            layout.Children.Add(label); 
+            layout.Children.Add(btn);
+
+
+            this.Content = layout;
+        }
+    }
+}
